d930a9eca0cfd33c718eba845a465af943f707e8
Vicente Barros <vmabarros@ua.pt>
2024-01-10 18:41:13 +0000

    voice handler cleanup

--- app/function_voice_handler.py
+++ app/function_voice_handler.py
@@ -1,69 +1,39 @@
-import json
-from os import system
-import time
-import xml.etree.ElementTree as ET
-import ssl
-import websockets
 from utils import *
-
+import logging
 
 from game.game import Game
 from dictionaries.dictionarys import houses,colors
 from lists.intent_list import list_intent
 
 intent_before = ""
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
+MIN_CONFIDENCE = 0.7
+available_colors_pt =["verde lima", "amarela", "laranja", "vermelho", "azul", "ciano", "verde", "castanha", "magenta", "cor de rosa"]
 
-async def voice_handler(game: Game, message:str):
+
+
+async def voice_handler(game: Game, message:dict):
     global intent_before
-    print(f"Speak voice received: {message}")
-    if message["intent"]["name"] in list_intent:
-        print(f"Message received/ intent: {message['intent']['name']}")
-        intent = message["intent"]["name"]
-        if message["intent"]["confidence"] < 0.7:
+
+    log.info(f"Speak voice received: {message}")
+    intent = message["intent"]["name"]
+    confidence = message["intent"]["confidence"]
+
+    if intent in list_intent:
+        log.info(f"Message received/ intent: {intent} / confidence: {confidence}")
+       
+        if confidence < MIN_CONFIDENCE:
             game.tts(random_not_understand())
         elif intent == "insert_name":#DONE  
-            if message["entities"]:
-                if len(message["entities"]) > 0:
-                    name = message["entities"][0]["value"].lower()
-                    game.insert_name(name)
-                else:
-                    game.tts(random_not_understand_name())
-            else:
-                game.tts(random_not_understand_name())
-            intent_before = intent
+            insert_name_handler(game, message, intent)
         elif intent == "create_room": # DONE
             game.create_game()
             intent_before = intent
         elif intent == "choose_color": # DONE
-            intent_before = intent
-            if message["entities"]:
-                if len(message["entities"]) > 0:
-                    color_name = message["entities"][0]["value"].lower()
-                    if color_name in colors:
-                        color_name = colors[color_name]
-                        print(f"Color name: {color_name}")
-                        game.choose_color(color_name)
-                    else:
-                        game.tts(random_not_valid_color())
-                else:
-                    game.tts(random_not_understand_color())
-            else:
-                game.tts(random_not_understand_color())
+            choose_color_handler(game, message, intent)
         elif intent == "information_house":
-            intent_before = intent
-            if message["entities"]:
-                if len(message["entities"]) > 0:
-                    house_name = message["entities"][0]["value"].lower()
-                    if house_name in houses:
-                        house_name = houses[house_name]
-                        print(f"House name: {house_name}")
-                        game.list_house_information(house_name)
-                    else:
-                        game.tts("O jogo não tem essa propriedade")
-                else:
-                    game.tts("Por favor, repita o nome da propriedade")
-            else:
-                game.tts("Por favor, diz o nome da propriedade")
+            information_house_handler(game, message, intent)
         elif intent == "start_game": #DONE
             game.start_game()
             intent_before = intent
@@ -95,8 +65,7 @@ async def voice_handler(game: Game, message:str):
             global not_quit
             not_quit = False
         elif intent == "list_of_colors": #DONE
-            colors_in_pt =["verde lima", "amarela", "laranja", "vermelho", "azul", "ciano", "verde", "castanha", "magenta", "cor de rosa"]
-            string_colors = ", ".join(colors_in_pt)
+            string_colors = ", ".join(available_colors_pt)
             game.tts(f"As cores disponíveis são: {string_colors}")
             intent_before = intent
         elif intent == "game_info": # DONE
@@ -110,7 +79,53 @@ async def voice_handler(game: Game, message:str):
             intent_before = intent
         else:
             game.tts(random_not_understand())
-            print(f"Command not found: {message}")
+            log.info(f"Command not found: {message}")
     else:
         game.tts(random_not_understand())
-        print(f"Command not found: {message}")
+        log.info(f"Command not found: {message}")
+
+def information_house_handler(game, message, intent):
+    global intent_before
+    intent_before = intent
+    if message["entities"]:
+        if len(message["entities"]) > 0:
+            house_name = message["entities"][0]["value"].lower()
+            if house_name in houses:
+                house_name = houses[house_name]
+                log.info(f"House name: {house_name}")
+                game.list_house_information(house_name)
+            else:
+                game.tts("O jogo não tem essa propriedade")
+        else:
+            game.tts("Por favor, repita o nome da propriedade")
+    else:
+        game.tts("Por favor, diz o nome da propriedade")
+
+def choose_color_handler(game, message, intent):
+    global intent_before
+    intent_before = intent
+    if message["entities"]:
+        if len(message["entities"]) > 0:
+            color_name = message["entities"][0]["value"].lower()
+            if color_name in colors:
+                color_name = colors[color_name]
+                log.info(f"Color name: {color_name}")
+                game.choose_color(color_name)
+            else:
+                game.tts(random_not_valid_color())
+        else:
+            game.tts(random_not_understand_color())
+    else:
+        game.tts(random_not_understand_color())
+
+def insert_name_handler(game, message, intent):
+    global intent_before
+    if message["entities"]:
+        if len(message["entities"]) > 0:
+            name = message["entities"][0]["value"].lower()
+            game.insert_name(name)
+        else:
+            game.tts(random_not_understand_name())
+    else:
+        game.tts(random_not_understand_name())
+    intent_before = intent
