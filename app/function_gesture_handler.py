f74d951d0a3532f519421c5cf67e7b28f68700fd
Vicente Barros <vmabarros@ua.pt>
2024-01-10 18:52:16 +0000

    Clean up gestures functions

--- app/function_gesture_handler.py
+++ app/function_gesture_handler.py
@@ -1,16 +1,20 @@
 
+import logging
 from game.game import Game
 from lists.gestures_list import list_gesture
 
-from utils import *
-from selenium.webdriver.common.by import By
+from utils import random_not_understand, random_not_understand_the_gesture
 
+MIN_CONFIDENCE = 0.7
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
 
 async def gesture_handler(game: Game, gesture:str):
-    print(f"Gesture received: {gesture}")
+    log.info(f"Gesture received: {gesture}")
     name_of_gesture = gesture["recognized"][1]
-    confidence = gesture["confidence"].replace(",", ".")
-    if float(confidence) < 0.7:
+    confidence = float(gesture["confidence"].replace(",", "."))
+
+    if confidence < MIN_CONFIDENCE:
         game.tts(random_not_understand())
     elif name_of_gesture in list_gesture:
         if name_of_gesture == "HANDRIGHTUPHELP":
@@ -18,24 +22,11 @@ async def gesture_handler(game: Game, gesture:str):
         elif name_of_gesture == "HANDSUPGIVEUP":
             game.give_up_game()
         elif name_of_gesture == "HANDSFRONTSELECT":
-            if game.button.join_game.text.lower() == "join game":
-                game.join_game()
-                if game.button.join_game.text.lower() != "join game":
-                    game.house_activate(number=1, increase=True)
-            else:
-                name_house = game.name_house
-                game.list_house_information(name_house)
+            hand_front_select_handler(game)
         elif name_of_gesture == "HANDLEFTSHOULDERLEVELDECREASE":
-            if game.button.join_game.text.lower() == "join game":
-                game.change_color_number(number=1, increase=False)
-            else:
-                game.house_activate(number=1, increase=False)
-
+            hand_left_shoulder_decrease_handler(game)
         elif name_of_gesture == "HANDRIGHTSHOULDERLEVELINCREASE":
-            if game.button.join_game.text.lower() == "join game":
-                game.change_color_number(number=1, increase=True)
-            else:
-                game.house_activate(number=1, increase=True)
+            hand_right_shoulder_increase_handler(game)
         elif name_of_gesture == "HANDSDIFFERENTDIRECTIONSCLOSE":    
             game.end_turn()
         elif name_of_gesture == "HANDONEDIRECTIONROLLDICE":
@@ -44,4 +35,25 @@ async def gesture_handler(game: Game, gesture:str):
             game.buy()
     else:
         game.tts(random_not_understand_the_gesture())
-        print(f"Command not found: {gesture}")
+        log.info(f"Command not found: {gesture}")
+
+def hand_right_shoulder_increase_handler(game):
+    if game.button.join_game.text.lower() == "join game":
+        game.change_color_number(number=1, increase=True)
+    else:
+        game.house_activate(number=1, increase=True)
+
+def hand_left_shoulder_decrease_handler(game):
+    if game.button.join_game.text.lower() == "join game":
+        game.change_color_number(number=1, increase=False)
+    else:
+        game.house_activate(number=1, increase=False)
+
+def hand_front_select_handler(game):
+    if game.button.join_game.text.lower() == "join game":
+        game.join_game()
+        if game.button.join_game.text.lower() != "join game":
+            game.house_activate(number=1, increase=True)
+    else:
+        name_house = game.name_house
+        game.list_house_information(name_house)
